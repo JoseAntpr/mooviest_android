a34c56e2ecd28eb5bfd2fb48d8403791e0b3205b
Jesús Garrido <jasus77@gmail.com>
2016-09-03 18:34:03 +0200

    RecyclerView and CelebritiesAdapter added

--- app/src/main/java/com/mooviest/ui/activities/MovieCastFragment.java
+++ app/src/main/java/com/mooviest/ui/activities/MovieCastFragment.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mooviest.R;
+import com.mooviest.ui.adapters.CelebritiesAdapter;
+import com.mooviest.ui.rest.SingletonRestClient;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +31,10 @@ public class MovieCastFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private RecyclerView recycler;
+    private GridLayoutManager layoutManager;
+    private CelebritiesAdapter celebritiesAdapter;
 
     //private OnFragmentInteractionListener mListener;
 
@@ -65,7 +73,18 @@ public class MovieCastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_cast, container, false);
+        View view = inflater.inflate(R.layout.fragment_movie_cast, container, false);
+
+        // RecyclerView
+        recycler = (RecyclerView) view.findViewById(R.id.celebrity_recycler);
+        layoutManager = new GridLayoutManager(getActivity(),3);
+        recycler.setLayoutManager(layoutManager);
+
+        celebritiesAdapter = new CelebritiesAdapter(SingletonRestClient.getInstance().movie_selected.getParticipations());
+
+        recycler.setAdapter(celebritiesAdapter);
+
+        return view;
     }
 
     /*
