6f7360b78043270f0fb5aad525c94e07c1c7c9b0
Jesús Garrido <jasus77@gmail.com>
2016-09-06 21:22:35 +0200

    Transparent status bar and image behind

--- app/src/main/java/com/mooviest/ui/activities/MovieDetailActivity.java
+++ app/src/main/java/com/mooviest/ui/activities/MovieDetailActivity.java
@@ -1,11 +1,18 @@
 package com.mooviest.ui.activities;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.transition.Slide;
 import android.view.Menu;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,27 +23,31 @@ import com.mooviest.ui.adapters.ViewPagerAdapter;
 import com.mooviest.ui.models.Lang_;
 import com.mooviest.ui.models.Movie;
 import com.mooviest.ui.rest.SingletonRestClient;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 public class MovieDetailActivity extends AppCompatActivity {
 
     ImageView cover_detail;
-    ImageView background_detail;
     TextView title_detail;
     TextView average_detail;
     TextView runtime_detail;
     ViewPager detail_pager;
     TabLayout detail_tabs;
+    CollapsingToolbarLayout collapsingToolbarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initActivityTransitions();
         setContentView(R.layout.activity_movie_detail);
+
+
 
         Movie m = SingletonRestClient.getInstance().movie_selected;
         Lang_ lang = m.getLangs().get(0);
         String image = lang.getImage();
-        String background;
+        final String background;
         String cover;
 
         if(image.startsWith("http")){
@@ -47,15 +58,41 @@ public class MovieDetailActivity extends AppCompatActivity {
             cover = "https://img.tviso.com/ES/poster/w430" + image;
         }
 
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar_detail));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle(lang.getTitle());
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(R.color.transparent));
+
+        // Load background image
+        final ImageView background_detail = (ImageView) findViewById(R.id.background_detail);
+        Picasso.with(this).load(background).into(background_detail, new Callback() {
+            @Override
+            public void onSuccess() {
+                Bitmap bitmap = ((BitmapDrawable) background_detail.getDrawable()).getBitmap();
+                Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                    public void onGenerated(Palette palette) {
+                        applyPalette(palette);
+                    }
+                });
+            }
+
+            @Override
+            public void onError() {
+
+            }
+        });
+        Picasso.with(this).setIndicatorsEnabled(false);
+
+
         // Load cover image
         cover_detail = (ImageView) findViewById(R.id.cover_detail);
         Picasso.with(this).load(cover).transform(new RoundedTransformation(10, 0)).into(cover_detail);
         Picasso.with(this).setIndicatorsEnabled(false);
 
-        // Load background image
-        background_detail = (ImageView) findViewById(R.id.background_detail);
-        Picasso.with(this).load(background).fit().centerCrop().into(background_detail);
-        Picasso.with(this).setIndicatorsEnabled(false);
+
 
         // Title
         title_detail = (TextView) findViewById(R.id.title_detail);
@@ -97,5 +134,22 @@ public class MovieDetailActivity extends AppCompatActivity {
         adapter.addFragment(new MovieCastFragment(), "Cast");
         adapter.addFragment(new MovieWatchFragment(), "Watch");
         viewPager.setAdapter(adapter);
+    }
+
+    private void applyPalette(Palette palette) {
+        int primaryDark = getResources().getColor(R.color.colorPrimaryDark);
+        int primary = getResources().getColor(R.color.colorPrimary);
+        collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(primary));
+        collapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkMutedColor(primaryDark));
+        supportStartPostponedEnterTransition();
+    }
+
+    private void initActivityTransitions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Slide transition = new Slide();
+            transition.excludeTarget(android.R.id.statusBarBackground, true);
+            getWindow().setEnterTransition(transition);
+            getWindow().setReturnTransition(transition);
+        }
     }
 }
